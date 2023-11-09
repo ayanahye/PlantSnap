e7b83041208fffce08f03325216805b1a8b42b5d
ayanahye <ayanahye2@gmail.com>
2023-11-09 13:15:32 -0800

    #13 uploaded sample data from APIs to test integration into different pages

--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -1,27 +1,38 @@
+'use client'
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react';
+import SampleData from "../SampleData.json";
 
 function page() {
 
+    /*
+
+    >>>>>> tested, and this works fine, sample data is in SampleData.json
+
+    const [data, setData] = useState(null);
+
     useEffect(() => {
         const getData = async () => {
-            const speciesId = 12;
-            const apiKey = '';
+            const speciesId = 1;
+            const apiKey = '---';
             const url = `https://perenual.com/api/species/details/${speciesId}?key=${apiKey}`;
             
             try {
                 const res = await fetch(url);
                 const data = await res.json();
                 console.log('response from API:', res);
+                setData(data);
             } catch (error) {
                 console.log("Error fetching data:", error);
             }
         }
         getData();
     }, []);
+
+    */
     
     return (
-        <div>page</div>
+        <div>{true && <div>{JSON.stringify(SampleData)}</div>}</div>
     )
 }
 
