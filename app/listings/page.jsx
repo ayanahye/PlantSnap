0d8d1207acbbe6d5be43a7414e07fcdea5af2223
ayanahye <ayanahye2@gmail.com>
2023-11-11 01:39:11 -0800

    #18 moved mapping to results page and added a default image if an image does not exist. Restricted the amount of results to 8 from the species list.

--- app/listings/page.jsx
+++ app/listings/page.jsx
@@ -3,51 +3,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import {useEffect} from 'react';
 import Image from 'next/image'; 
-import flowerImage from './temp-flower.jpg'; 
+import defaultImg from "./placeholder-img.jpg"
 import './Listings.css';
+import dataObject from "../SampleList.json";
 
-const cardData = [
-  {
-    imageSrc: flowerImage, 
-    title: 'Card Title 1',
-    description: 'This is a short card description 1.',
-  },
-  {
-    imageSrc: flowerImage, 
-    title: 'Card Title 2',
-    description: 'This is a short card description 2.',
-  },
-  {
-    imageSrc: flowerImage, 
-    title: 'Card Title 1',
-    description: 'This is a short card description 1.',
-  },
-  {
-    imageSrc: flowerImage, 
-    title: 'Card Title 2',
-    description: 'This is a short card description 2.',
-  },
-  {
-    imageSrc: flowerImage, 
-    title: 'Card Title 1',
-    description: 'This is a short card description 1.',
-  },
-  {
-    imageSrc: flowerImage, 
-    title: 'Card Title 2',
-    description: 'This is a short card description 2.',
-  },
-  {
-    imageSrc: flowerImage,
-    title: 'Card Title 1',
-    description: 'This is a short card description 1.',
-  },
-  {
-    imageSrc: flowerImage, 
-    title: 'Card Title 2',
-    description: 'This is a short card description 2.',
-  },
-];
+<ul>
+  {dataObject && dataObject.data.map(item => (
+      <li key={item.id}>
+          ID: {item.id}, Plant Name: {item.common_name}
+          {item.default_image && (
+              <img src={item.default_image.regular_url} alt={item.common_name} width="100" />
+          )}
+      </li>
+  ))}
+</ul>
+
+
 
 function filtering() {
 
@@ -55,17 +26,25 @@ function filtering() {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
+  const displayedData = dataObject.data.slice(0, 8);
+
   return (
     <div className="container-custom">
-      <p>Image search result: {cardData.length} results</p>
+      <p>Image search result: {displayedData.length} results</p>
       <div className="row row-cols-1 row-cols-md-4">
-        {cardData.map((card, index) => (
-          <div className="col mb-4" key={index}>
+        {displayedData.map((item, index) => (
+          <div className="col mb-4" key={item.id}>
             <div className="card h-100">
-              <Image src={card.imageSrc} width={500} height={200} className="card-img-top" alt="..." />
+              {item.default_image ? (
+                <Image src={item.default_image.regular_url} width={500} height={200} className="card-img-top" alt={item.common_name} />
+              ) : (
+                <Image src={defaultImg} width={500} height={200} className="card-img-top" alt={item.common_name} />
+              )
+            
+            }
               <div className="card-body">
-                <h5 className="card-title">{card.title}</h5>
-                <p className="card-text">{card.description}</p>
+                <h5 className="card-title">{item.common_name}</h5>
+                <p className="card-text">ID: {item.id}</p>
               </div>
             </div>
           </div>
