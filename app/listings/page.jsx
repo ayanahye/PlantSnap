b33977c44ca3a6d822c571f46750820373ae9030
ayanahye <ayanahye2@gmail.com>
2023-11-10 23:47:24 -0800

    #19 added a bit of styling and structure to the results page, more styling needed. Used map function and added data to an array of objects.

--- app/listings/page.jsx
+++ app/listings/page.jsx
@@ -1,21 +1,72 @@
 'use client'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.bundle';
-import Nav from '../components/nav';
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import Image from 'next/image'; 
+import flowerImage from './temp-flower.jpg'; 
+import './Listings.css';
 
-import React from 'react'
-import '../styles/App.css';
-import '../styles/Listings.css'
+const cardData = [
+  {
+    imageSrc: flowerImage, 
+    title: 'Card Title 1',
+    description: 'This is a short card description 1.',
+  },
+  {
+    imageSrc: flowerImage, 
+    title: 'Card Title 2',
+    description: 'This is a short card description 2.',
+  },
+  {
+    imageSrc: flowerImage, 
+    title: 'Card Title 1',
+    description: 'This is a short card description 1.',
+  },
+  {
+    imageSrc: flowerImage, 
+    title: 'Card Title 2',
+    description: 'This is a short card description 2.',
+  },
+  {
+    imageSrc: flowerImage, 
+    title: 'Card Title 1',
+    description: 'This is a short card description 1.',
+  },
+  {
+    imageSrc: flowerImage, 
+    title: 'Card Title 2',
+    description: 'This is a short card description 2.',
+  },
+  {
+    imageSrc: flowerImage,
+    title: 'Card Title 1',
+    description: 'This is a short card description 1.',
+  },
+  {
+    imageSrc: flowerImage, 
+    title: 'Card Title 2',
+    description: 'This is a short card description 2.',
+  },
+];
 
 function filtering() {
   return (
-    <div className="App">
-         <Nav />
-         <div class="container-custom">
-            <h2 class="big">Listings page</h2>
-        </div>
+    <div className="container-custom">
+      <p>Image search result: {cardData.length} results</p>
+      <div className="row row-cols-1 row-cols-md-4">
+        {cardData.map((card, index) => (
+          <div className="col mb-4" key={index}>
+            <div className="card h-100">
+              <Image src={card.imageSrc} width={500} height={200} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{card.title}</h5>
+                <p className="card-text">{card.description}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default filtering
+export default filtering;
