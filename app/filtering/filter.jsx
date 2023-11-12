24028b566cd67ed4e700c42454c80b59d35669be
ayanahye <ayanahye2@gmail.com>
2023-11-11 21:30:31 -0800

    #20 finished adding the API request to the page.jsx file, it is passed as a prop to filter.jsx and the dropdowns holding options are updated there.

--- app/filtering/filter.jsx
+++ app/filtering/filter.jsx
@@ -129,7 +129,7 @@ export default function Filter(props) {
     
     
     return (
-        <main>
+        props.speciesList && <main>
             <h3 style={{paddingBottom: '15px'}}>Current plant = {props.speciesList.common_name || "No name yet"}</h3>
             <div className="filterPart">
                 <div className="images">
--- app/filtering/page.jsx
+++ app/filtering/page.jsx
@@ -3,19 +3,45 @@ import { useEffect, useState } from "react";
 
 import React from 'react'
 import Filter from './filter';
-import Data from '../SampleData.json';
+import speciesData from '../SampleData.json';
 import '../styles/App.css';
 
 function Filtering() {
+
+  /*
+
+  const [speciesData, setSpeciesData] = useState(null);
+
+  useEffect(() => {
+      const getData = async () => {
+          const speciesId = 15;
+          const apiKey = 'sk-sZxb6546cce0783d62824';
+          const url = `https://perenual.com/api/species/details/${speciesId}?key=${apiKey}`;
+          
+          try {
+              const res = await fetch(url);
+              const data = await res.json();
+              console.log('response from API:', res);
+              setSpeciesData(data);
+          } catch (error) {
+              console.log("Error fetching data:", error);
+          }
+      }
+      getData();
+  }, []);
+
+
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
-  const [speciesList, setSpeciesList] = useState(Data);
+  // const [speciesList, setSpeciesList] = useState(Data);
+
+  */
 
   return (
     <div className="App">
-        <Filter speciesList={speciesList} />
+        <Filter speciesList={speciesData} />
     </div>
 
 
